9f6f7017a417b50d082deb76d8ca5839c182e6cd
Michał Kuliga <mkuliga72@gmail.com>
2024-10-06 23:18:31 +0200

    Adding database connection

---
+++ Models/Category.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models;
+
+public class Category
+{
+    public int CategoryId { get; set; }
+    [Required]
+    public string Name { get; set; }
+    public int DisplayOrder { get; set; }
+}
