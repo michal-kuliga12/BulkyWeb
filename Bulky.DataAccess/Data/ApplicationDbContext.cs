f5fa96cf6d3b4c95f3684b231c4ccc0e085e4607
Michał Kuliga <mkuliga72@gmail.com>
2024-10-21 21:47:18 +0200

    New project structure, corrected repository base directory

---
+++ Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Bulky.Models;
+using Microsoft.EntityFrameworkCore;
+namespace Bulky.DataAccess.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<Category> Categories { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>().HasData(
+            new Category { CategoryId = 1, Name = "Action", DisplayOrder = 1 },
+            new Category { CategoryId = 2, Name = "Action1", DisplayOrder = 2 },
+            new Category { CategoryId = 3, Name = "Action2", DisplayOrder = 3 },
+            new Category { CategoryId = 4, Name = "Action3", DisplayOrder = 4 }
+            );
+    }
+}
---
+++ Bulky.Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Bulky.Models;
+
+public class Category
+{
+    [Required]
+    public int CategoryId { get; set; }
+    [Required]
+    [MaxLength(100)]
+    [DisplayName("Category Name")]
+    public string Name { get; set; }
+    [DisplayName("Display Order")]
+    [Range(1, 100, ErrorMessage = "Dislpay order must be between 1-100")]
+    public int DisplayOrder { get; set; }
+}
